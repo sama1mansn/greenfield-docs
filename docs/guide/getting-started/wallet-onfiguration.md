ef1ac397d56c3822fe557e892c36cb6b79cdd86f
RumeelHussainbnb <rumeelhussainbnb@gmail.com>
2023-05-30 23:15:19 +0500

    first commit

---
+++ docs/guide/getting-started/wallet-onfiguration.md
@@ -0,0 +1,43 @@
+---
+title: Wallet Configuration
+order: 2
+---
+
+# Wallet Configuration
+
+In this guide, you will learn how to use the MetaMask extension to interact with Greenfield Testnet and
+Greenfield BSC Testnet, which is a fork of BSC and deployed dedicated for Greenfield Testnet.
+
+We assume you have installed MetaMask and have an account, if not, please refer to [MetaMask](https://metamask.io/download/)
+to install it.
+
+## Add BSC Testnet Network
+1. Visit [BNBChain List](https://www.bnbchainlist.org/).
+
+2. Click **Connect Wallet**.
+
+<img src="/asset/201-Connect-Wallet.png"/>
+
+3. Find **BNB Smart Chain Testnet**, and click **Add To Metamask**.
+
+<img src="/asset/202-Greenfield-BSC-Testnet-Chain-List.png" style={{width:"60%"}}/>
+
+4. When MetaMask prompts a window, click **Approve**.
+
+<img alt="add-bsc-testnet" src="/asset/203-Approve-Add-Greenfield-BSC-Testnet.png" style={{width:"40%"}}/>
+
+
+## Add Greenfield Testnet Network
+1. Visit [BNBChain List](https://www.bnbchainlist.org/).
+2. Click **Connect Wallet** if wallet didn't connect.
+
+<img alt="connect-wallet" src="/asset/201-Connect-Wallet.png" style={{width:"40%"}}/>
+
+3. Find **Greenfield Blockchain Testnet**, and click **Add To Metamask**
+
+<img alt="greenfield-chain-list" src="/asset/204-Greenfield-Testnet-Chain-List.png" style={{width:"40%"}}/>
+
+4. When MetaMask prompts a window, click **Approve**
+
+<img at="approve-greenfield-testnet" src="/asset/205-Approve-Add-Greenfield-Testnet.png" style={{width:"40%"}}/>
+
---
+++ docusaurus.config.js
@@ -0,0 +1,190 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "BNB Greenfield",
+  tagline: "Dinosaurs are cool",
+  url: "https://your-docusaurus-test-site.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "RumeelHussainbnb", // Usually your GitHub org/user name.
+  projectName: "bnbgreenfield-docs", // Usually your repo name.
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          docLayoutComponent: "@theme/DocPage",
+          docItemComponent: "@theme/ApiItem" // Derived from docusaurus-theme-openapi
+        },
+        blog: {
+          showReadingTime: true,
+          editUrl: "https://www.bnbchain.org/en/blog/",
+
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css")
+        }
+      })
+    ]
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      docs: {
+        sidebar: {
+          hideable: true
+        }
+      },
+      navbar: {
+        title: "BNB Greenfield",
+        logo: {
+          alt: "Greenfield Logo",
+          src: "img/logo.svg"
+        },
+        items: [
+          {
+            label: "Guide",
+            position: "left",
+            to: "/docs/guide/introduction/overview"
+          },
+          {
+            label: "API Reference",
+            position: "left",
+            to: "/docs/api-sdk/grpc-rest"
+          },
+          {
+            label: "API Playground",
+            position: "left",
+            to: "/docs/greenfield-api"
+          },
+          {
+            label: "Release Notes",
+            position:"left",
+            to: "/docs/release-notes/releaseNotes"
+          },
+         // {to: 'blog', label: 'Changelog', position: 'left'},
+          {
+            label: "FAQs",
+            position: "left",
+            to: "/docs/faq/greenfield-faqs"
+          },
+          {
+            href: 'https://github.com/facebook/docusaurus',
+            position: 'right',
+            className: 'header-github-link',
+            'aria-label': 'GitHub repository',
+          },
+        ]
+      },
+      footer: {
+        /*style: "dark",
+        logo: {
+          alt: 'BNB Chain Logo',
+          src: 'img/logo.svg',
+          width: 160,
+          height: 51,
+        },
+        links: [
+          {
+            title:"Learn",
+            items:[              
+              { 
+                label: "Whitepaper",
+                to: "https://github.com/bnb-chain/greenfield-whitepaper"
+              },
+              { 
+                label: "Official Website",
+                to: "http://greenfield.bnbchain.org/"
+              },
+            ]
+          },
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Discord",
+                href: "https://discord.com/invite/QRTQvfhADQ"
+              },
+              {
+                label: "Twitter",
+                href: "https://twitter.com/BNBChain"
+              },
+              {
+                label: 'Telegram',
+                href: 'http://t.me/bnbchain',              
+              }
+            ]
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "Blog",
+                to: "https://www.bnbchain.org/en/blog/"
+              },
+              {
+                label:"Youtube",
+                to: "https://www.youtube.com/channel/UCG9fZu6D4I83DStktBV0Ryw"
+              },
+              {
+                label: "GitHub",
+                href: "https://github.com/bnb-chain"
+              },
+            ]
+          }
+        ],
+        
+        copyright: `Copyright © ${new Date().getFullYear()} BNB Chain`
+      */
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+        additionalLanguages: ["ruby", "csharp", "php"]
+      }
+    }),
+
+  plugins: [
+    [
+      "docusaurus-plugin-openapi-docs",
+      {
+        id: "openapi",
+        docsPluginId: "classic",
+        config: {
+          greenfield: {
+            specPath: "swagger/greenfield-api.yaml",
+            outputDir: "docs/greenfield-api",
+            downloadUrl:
+              "https://raw.githubusercontent.com/PaloAltoNetworks/docusaurus-template-openapi-docs/main/examples/petstore.yaml",
+           sidebarOptions: {
+              groupPathsBy: "tag",
+              categoryLinkSource: "tag"
+            }
+          }
+        }
+      }
+    ]
+  ],
+
+  themes: ["docusaurus-theme-openapi-docs"]
+};
+
+module.exports = config;
