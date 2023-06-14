6595386e6c7b0607f62990acdf20d76dbd317074
RumeelHussainbnb <93580180+RumeelHussainbnb@users.noreply.github.com>
2023-06-14 11:13:52 +0200

    Update landing page link in index.js (#86)

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import  { Redirect } from 'react-router-dom';
 
 export default function Home() {
-  return <Redirect to='/bnbgreenfield-docs/docs/guide/home' />;
+  return <Redirect to='/greenfield-docs/docs/guide/home' />;
 }
