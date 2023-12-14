66b5c033750c8ab739f6c36e4008823538af32fb
NodeTinker <134990340+maxnodereal@users.noreply.github.com>
2023-12-14 00:33:47 -0600

    add dev get started (#288)
    * add dev get started
    
    * resolve comments

--- sidebars.js
+++ sidebars.js
@@ -39,6 +39,7 @@ const sidebars = {
         "guide/getting-started/network-info",
         "guide/getting-started/get-test-bnb",
         "guide/getting-started/greenfield-command",
+        "guide/getting-started/get-started-dev",
       ],
     },
     {
